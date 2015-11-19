36476a6ac5014f86d055f52a5ddb965d484953f3
Paul de Wouters <paul@hmn.md>
2015-11-19 09:19:45 +0000

    Use constant for version

--- load-scripts.php
+++ load-scripts.php
@@ -5,7 +5,7 @@ add_action( 'wp_enqueue_scripts','baw_load_scripts' );
 
 function baw_load_scripts() {
 
-	wp_register_script( 'baw-script',plugins_url( '/baw-script.min.js', __FILE__ ), array( 'jquery' ), '1.0', true );
+	wp_register_script( 'baw-script',plugins_url( '/baw-script.min.js', __FILE__ ), array( 'jquery' ), BAW_VERSION, true );
 	wp_enqueue_script( 'baw-script' );
 
 }
